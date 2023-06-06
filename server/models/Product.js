9eea042a6c010207e4cf7242f7b16302f79e5935
LaloEnri23 <126302462+LaloEnri23@users.noreply.github.com>
2023-06-06 15:56:54 -0700

    modals reborn

---
+++ server/models/Product.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const { Schema, model } = require('mongoose');
+
+const productSchema = new Schema(
+  {
+  name: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true
+},
+category: {
+  type: String,
+  required: true,
+  trim: true
+},
+price: {
+  type: Number,
+  required: true,
+  trim: true
+},
+quantity: {
+  type: Number,
+  required: true,
+  trim: true
+},
+image: {
+  type: String,
+  required: true,
+  trim: true
+},
+
+}
+);
+
+const Product = model('Product', productSchema);
+
+module.exports = Product;
